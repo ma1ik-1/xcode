e28bb370f589e92d3555f98c072115fe8f585c9a
ma1ik-1 <amalik4@qa.com>
2023-05-24 10:19:29 +0100

    first xcode example

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Malik on 24/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
